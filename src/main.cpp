77d280755b7c70f031e7eabc0abeb1a8ae5eadf5
Ivan <rexx.magtar@gmail.com>
2022-01-12 05:07:34 +0300

    code cleanup[A

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
-#include <bits/stdc++.h>
+#include <fastcgi++/manager.hpp>
+#include "webServer.h"
 
-int
+int 
 main()
 {
+    Fastcgipp::Manager<webServer> manager;
+    manager.setupSignals();
+    manager.listen();
+    manager.start();
+    manager.join();
+
+    return 0;
 }
